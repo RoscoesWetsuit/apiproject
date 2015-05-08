1d17199896abc93a4845e754942662ae62669dd1
Cade Guevara <cadeguevara@yahoo.com>
2015-05-07 20:42:47 -0700

    finished milestone 2, we just made some constants

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+//configuration for our PHP Server
+set_time_limit(0);
+ini_set('default_socket_timeout', 300);
+session_start();
+
+//make constants using define
+define('client_id', 'fba0fe2c6a004fbbb3ee10418800abdd');
+define('client_secret', 'fba0fe2c6a004fbbb3ee10418800abdd');
+define('redirectURI', 'http://localhost/apiproject/index.php');
+define('ImageDirectory', 'pics/');
+
+?>
+
+<!--CLIENT INFO
+CLIENT ID	fba0fe2c6a004fbbb3ee10418800abdd
+CLIENT SECRET	7c5dfa3acec045a0b2accd52499f73a6
+WEBSITE URL	http://localhost/apiproject/index.php
+REDIRECT URI	http://localhost/apiproject/index.php
+-->
+
