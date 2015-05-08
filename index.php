942fd5f1b39fa8c0c62bccee083dd0953af4d0dd
Cade Guevara <cadeguevara@yahoo.com>
2015-05-07 21:38:37 -0700

    finished milestone 4, still have an error though.

--- index.php
+++ index.php
@@ -5,8 +5,8 @@ ini_set('default_socket_timeout', 300);
 session_start();
 
 //make constants using define
-define('client_ID', 'fba0fe2c6a004fbbb3ee10418800abdd');
-define('client_Secret', 'fba0fe2c6a004fbbb3ee10418800abdd');
+define('clientID', 'fba0fe2c6a004fbbb3ee10418800abdd');
+define('client_Secret', '7c5dfa3acec045a0b2accd52499f73a6');
 define('redirectURI', 'http://localhost/apiproject/index.php');
 define('ImageDirectory', 'pics/');
 
@@ -21,6 +21,6 @@ define('ImageDirectory', 'pics/');
 	<!-- creating a login for people to goand give aproval for our web app to access their instagram account
 	after getting aproval, we are now going to ave the information so that we can play with it/
 	 -->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">login</a>
+	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI; ?>&response_type=code">login</a>
 </body>
 </html>
