c296909ed380dde65a10d9a364280b0907539536
Cade Guevara <cadeguevara@yahoo.com>
2015-05-13 12:09:51 -0700

    finished milestone 10, created function to get our user id.

--- index.php
+++ index.php
@@ -14,16 +14,25 @@ define('ImageDirectory', 'pics/');
 function connectToInstagram($url){
 	$ch = curl_init();
 
-	curl_setopt_array($ch, array{
-		CURLOPT_URL = $url;
-		CURLOPT_RETURNTRANSFER = true;
-		CURLOPT_SSL_VERIFYPEER = false,
-		CURLOPT_SSL_VERIFYHOST = 2,
-	});
+	curl_setopt_array($ch, array( 
+		CURLOPT_URL => $url,
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_SSL_VERIFYPEER => false,
+		CURLOPT_SSL_VERIFYHOST => 2,
+	));
 	$result = curl_exec($ch);
 	curl_close($ch);
-
+	return $result;
 }
+//Function to get userID cause username doesnt allow us to get pictures!
+function getUserID($userName){
+	$url = 'http://api.instagram.com/v1/users/search?q'.$userName.'&client_id='.clientID;
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+
+	echo $results['data']['0']['id'];
+}
+
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
 	$url = 'https://api.instagram.com/oauth/access_token';
@@ -43,8 +52,8 @@ curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true); //but in live work-production 
 $result = curl_exec($curl);
 curl_close($curl);
 
-$result = json_decode($results, true);
-echo $results['user']['username'];
+$results = json_decode($result, true);
+getUserID($results['user']['username']);
 }
 else{
 ?>
