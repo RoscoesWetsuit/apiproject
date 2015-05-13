4e1b666b58731f635bcc3724c98066d171f077c7
Cade Guevara <cadeguevara@yahoo.com>
2015-05-12 18:17:26 -0700

    finished milestone 7, using curl to talk to other websites to recieve information.

--- index.php
+++ index.php
@@ -19,8 +19,17 @@ if (isset($_GET['code'])){
 									'redirect_uri' => redirectURI,
 									'code' => $code
 									);
-}
+//CURL is what we use in PHP, It's a library that calls to other API's
+$curl = curl_init($url); //setting a curl session and we put in $curl because that's where we are getting the data from
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); //setting the POSTFIELDS to the array setup that we created
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting it to eaual to 1 because we are getting strings back.
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true); //but in live work-production we want to set this to true
 
+
+}
+$result = curl_exec($curl);
+curl_close();
 ?>
 
 <!DOCTYPE html>
