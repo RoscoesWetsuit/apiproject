bee2622f1d1cdc4046ccb9688691568c5873b306
Cade Guevara <cadeguevara@yahoo.com>
2015-05-14 12:01:40 -0700

    finished milestone 11

--- index.php
+++ index.php
@@ -32,6 +32,18 @@ function getUserID($userName){
 
 	echo $results['data']['0']['id'];
 }
+//function to print out images onto our screen
+function printImages($userID){
+	$url = 'https://api.instagram.com/v1/users/'.$userID.'/media/recent?client_id='.clientID.'&count=S';
+	$instagramInfo = connectToInstagram($url);
+	$results = json_decode($instagramInfo, true);
+	//parse through the information one by one.
+	foreach($results['data'] as $items){
+		$image_url = $items['images']['low_resolution']['url']; //going to go through all of my results and give myself back the URL of those pictures
+		//because we went to save it in the PHP sever. 
+		echo '<img src=" '.$image_url.'"/><br/>';
+	}
+}
 
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
@@ -49,11 +61,17 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $access_token_settings); //setting the PO
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); //setting it to eaual to 1 because we are getting strings back.
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, true); //but in live work-production we want to set this to true
 
+
 $result = curl_exec($curl);
 curl_close($curl);
 
 $results = json_decode($result, true);
-getUserID($results['user']['username']);
+
+$userName = $results['user']['username'];
+
+$userID = getUserID($userName);
+
+printImages($userID);
 }
 else{
 ?>
